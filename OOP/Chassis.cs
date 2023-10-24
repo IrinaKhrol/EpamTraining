2284c6dc91f40028215df9184382901b8fad0752
IrinaKhrol <120750891+IrinaKhrol@users.noreply.github.com>
2023-10-24 07:52:28 -0700

    add OOP (#3)

---
+++ OOP/Chassis.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP
+{
+    public class Chassis
+    {
+        public int NumberOfWheels { get; set; }
+
+        public string ChassisNumber { get; set; }
+
+        public int LoadCapacity { get; set; }
+
+        public Chassis(int numberOfWheels, string chassisNumber, int loadCapacity)
+        {
+            NumberOfWheels = numberOfWheels;
+            ChassisNumber = chassisNumber;
+            LoadCapacity = loadCapacity;
+        }
+
+        public string GetInformationAboutChassis()
+        {
+            return $"'Chassis' - Number Of Wheels: {NumberOfWheels}, Chassis Number: {ChassisNumber}, Load Capacity: {LoadCapacity}\n";
+        }
+    }
+}
