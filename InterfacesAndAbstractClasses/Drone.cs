710c4902fe54fb0379638a675931651f50d0a41a
IrinaKhrol <120750891+IrinaKhrol@users.noreply.github.com>
2023-10-24 07:53:39 -0700

    add InterfacesAndAbstractClasses project (#4)

---
+++ InterfacesAndAbstractClasses/Drone.cs
@@ -0,0 +1,57 @@
+﻿namespace InterfacesAndAbstractClasses
+{
+    internal class Drone : FlyingObject, IFlyable
+    {
+
+        const int MinSpeedKm = 50;
+        const int MaxSpeedKm = 300;
+        const double FreezeTime = 0.0166666; // 1min = 1/60h = 0.0166666h (time the drone hangs in the air)
+        const double IntervalTimeBeforeFreeze = 0.166666; // 10 min = 1/6h = 0.166666h(time interval after which the drone hovers in the air again)
+        const int MaxDistance = 500;// maximum possible flight distance of the drone
+
+        public Drone(Coordinate currentCoordinate)
+        {
+            this.currentCoordinate = currentCoordinate;
+            this.speed = MinSpeedKm;
+        }
+
+        public Drone()
+        {
+            this.speed = GetSpeed();
+        }
+
+        private int GetSpeed()
+        {
+            Random rnd = new Random();
+            return rnd.Next(MinSpeedKm, MaxSpeedKm);
+        }
+
+        public void FlyTo(Coordinate newCoordinate)
+        {
+            double distance = Math.Round(GetDistance(newCoordinate), 2);
+
+            if (ValidateMaxDistance(newCoordinate))
+            {
+                CurrentCoordinate = newCoordinate;
+            }
+            else
+            {
+                Console.WriteLine($"New Coordinate can't be reached out by the drone due to the dictance = {distance} exceeds the Max Distance restriction = {MaxDistance} km");
+            }
+            CurrentCoordinate = newCoordinate;
+        }
+
+        public double GetFlyTime(Coordinate newCoordinate)
+        {
+            double distance = GetDistance(newCoordinate);
+            double timeWithoutFreezes = distance / Speed; // flight time without hovering in hours
+            int freezes = (int)(timeWithoutFreezes / IntervalTimeBeforeFreeze);// total number of hovering in the air
+            return Math.Round(timeWithoutFreezes + (freezes * FreezeTime), 2);// total flight time = free flight time + (number of hangs multiplied by the time of one hang)
+        }
+
+        public bool ValidateMaxDistance(Coordinate newCoordinate)
+        {
+            return (GetDistance(newCoordinate) >= MaxDistance) ? false : true;
+        }
+    }
+}
---
+++ InterfacesAndAbstractClasses/FlyingObject.cs
@@ -0,0 +1,27 @@
+﻿namespace InterfacesAndAbstractClasses
+{
+    abstract class FlyingObject
+    {
+        protected Coordinate currentCoordinate;
+        protected int speed;
+        protected double passedDistance;
+
+
+        protected int Speed { get { return speed; } set { speed = value; } }
+        protected Coordinate CurrentCoordinate { get { return currentCoordinate; } set { currentCoordinate = value; } }
+        protected double PassedDistance { get; set; } = 0;
+
+        protected double GetDistance(Coordinate newCoordinate)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(newCoordinate.X - currentCoordinate.X, 2) +
+                Math.Pow(newCoordinate.Y - currentCoordinate.Y, 2) +
+                Math.Pow(newCoordinate.Z - currentCoordinate.Z, 2)),2) + PassedDistance;
+        }
+
+        public void PrintDistance(Coordinate newCoordinate)
+        {
+            Console.WriteLine($"The distance from the currect coordinate {currentCoordinate.PrintCoordinate()} to new coordinate: {newCoordinate.PrintCoordinate()} equals {GetDistance(newCoordinate)} km");
+        }
+
+    }
+}
